93cba9a03e5841ed724c245fc102768673808699
princeraichura <raichura72@gmail.com>
2023-05-12 16:42:16 +0530

    Task for evaluation using IPC

---
+++ task1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+int main() {
+  int msgid = msgget(55,IPC_CREAT|0666);
+struct mes{
+    char name[100];
+    int marks;
+    int rn;
+}stu;
+  
+printf("Enter the student name: ");
+scanf("%s",stu.name);
+//fgets(stu.name,sizeof(stu.name),stdin);
+printf("enter student marks: ");
+scanf("%d",&stu.marks);
+printf("Enter the student roll number: ");
+scanf("%d", &stu.rn);
+
+msgsnd(msgid, &stu, sizeof(stu), 0);
+
+msgctl(msgid, IPC_RMID, NULL);
+
+  exit(EXIT_SUCCESS);
+}
---
+++ task2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+int main() {
+  int msgid = msgget(55,IPC_CREAT|0666);
+ 
+  struct mes{
+    char name[1024];
+    int marks;
+    int rn;
+  }stu;
+int rd=msgrcv(msgid,&stu,sizeof(stu),0,0);
+
+  printf("The student record is: %s,%d,%d\n", stu.name, stu.marks, stu.rn);
+  msgctl(msgid, IPC_RMID, NULL);
+  exit(1);
+  
+}
+
